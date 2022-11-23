73238d73dd0e3414c9b4a2de78d3e425beef8d4f
yasar-restore <117475407+yasar-restore@users.noreply.github.com>
2022-11-23 23:27:17 +0000

    Update GildedRoseTest.cs
    initial commit

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -12,7 +12,7 @@ namespace GildedRoseTests
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal("foo", Items[0].Name);
         }
     }
 }
